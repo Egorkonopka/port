97c6b2c88f7aff7719fc13681cfb408fe9de2a53
egor <egorkonopka93@gmail.com>
2020-10-02 15:23:26 +0300

    first commit port

---
+++ views/category/dislocation.php
@@ -0,0 +1,23 @@
+
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-7 item">
+				<div class="item active">
+				<h2>Где мы находимся</h2>
+ 				</div>
+ 				<p style="text-align: left;">улица Портовая, 2а, Каменское, Днепропетровская область</p>
+ 				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d330.2606886396744!2d34.594604409183816!3d48.53159298661597!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1suk!2sua!4v1601547010136!5m2!1suk!2sua" width="500" height="380" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+			</div>
+			<div class="col-sm-5 item">
+				<div>
+					<img src="/images/home/place.jpg" alt="" />
+				</div>
+				<div>
+					<p>текст текст текст текст текст текст текст текст текст текст текст текст текст текст текст текст текст текст текст текст текст </p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
